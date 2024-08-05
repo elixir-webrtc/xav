61c860713b86fbe4e5f613ef772c90d90552ffb7
Michał Śledź <michalsledz34@gmail.com>
2024-08-05 13:20:58 +0200

    Make tests async again

--- test/reader_test.exs
+++ test/reader_test.exs
@@ -1,5 +1,5 @@
 defmodule Xav.ReaderTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
 
   test "new/1" do
     assert {:ok, %Xav.Reader{}} = Xav.Reader.new("./test/fixtures/sample_h264.mp4")
