b7efda3f2ea33dc7c46d137baf6ee2b363d556ef
Kevin Schweikert <54439512+kevinschweikert@users.noreply.github.com>
2025-11-26 21:22:47 +0100

    Add support for MacOS devices in reader (#42)
    * feat: add support for avfoundation (macOS)
    
    * feat: add framerate and resolution selection to device input
    
    * remove custom os flag from Makefile
    
    * refactor: move driver definitions to top of file
    
    * feat: remove default settings for framerate and video_size
    
    * refactor: use explixit defined for every os flag

--- lib/xav/reader_nif.ex
+++ lib/xav/reader_nif.ex
@@ -9,7 +9,7 @@ defmodule Xav.Reader.NIF do
     :ok = :erlang.load_nif(path, 0)
   end
 
-  def new(_path, _device, _video, _out_format, _out_sample_rate, _out_channels),
+  def new(_path, _device, _video, _out_format, _out_sample_rate, _out_channels, _framerate, _width, _height),
     do: :erlang.nif_error(:undef)
 
   def next_frame(_reader), do: :erlang.nif_error(:undef)
